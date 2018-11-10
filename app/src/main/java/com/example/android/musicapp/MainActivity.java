9369381d898db61e94b50a32ec98704a55699eda
jkiley129 <josephkiley@icloud.com>
2018-11-10 15:17:49 -0500

    iterating on layout

--- app/src/main/java/com/example/android/musicapp/MainActivity.java
+++ app/src/main/java/com/example/android/musicapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.android.musicapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -16,20 +17,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        words.add(new Word("father", "әpә"));
-        words.add(new Word("mother", "әṭa"));
-        words.add(new Word("son", "angsi"));
-        words.add(new Word("daughter", "tune"));
-        words.add(new Word("older brother", "taachi"));
-        words.add(new Word("younger brother", "chalitti"));
-        words.add(new Word("older sister", "teṭe"));
-        words.add(new Word("younger sister", "kolliti"));
-        words.add(new Word("grandmother", "ama"));
-        words.add(new Word("grandfather", "paapa"));
+        songs.add(new Song("Faint", "Linkin Park", ""));
 
-        ArrayAdapter<Word> itemsAdapter = new WordAdapter(this, words);
+        ArrayAdapter<Song> itemsAdapter = new SongAdapter(this, songs);
 
-        ListView listView = findViewById(R.id.list);
+        ListView listView = findViewById(R.);
 
         listView.setAdapter(itemsAdapter);
     }
